d0b7b50a7a767dae68e01651aa8e34304577ca27
Hugo Esteban Hernández Beltrán <hernandez.beltran.hugo.esteban@gmail.com>
2023-09-29 00:52:09 -0600

    Se agregan los cambios hasta el punto 18 de la práctica 4

--- src/time.js
+++ src/time.js
@@ -1,7 +1,8 @@
 function time(seconds) {
     // here is where you write your code...
-    if (seconds === 60) {
-        return "1m";
+    if (seconds >= 60) {
+        const minutes = seconds / 60;
+        return minutes + "m";
       }
     return seconds + "s";
   }
