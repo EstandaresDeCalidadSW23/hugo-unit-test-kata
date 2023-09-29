d27cd3423708c240a532f0aaf23de62bd690213d
Hugo Esteban Hernández Beltrán <hernandez.beltran.hugo.esteban@gmail.com>
2023-09-29 00:44:39 -0600

    Se agregan las pruebas para los minutos

--- src/time.js
+++ src/time.js
@@ -1,5 +1,8 @@
 function time(seconds) {
     // here is where you write your code...
+    if (seconds === 60) {
+        return "1m";
+      }
     return seconds + "s";
   }
   
--- test/time.test.js
+++ test/time.test.js
@@ -23,3 +23,51 @@ test("should return '1s' for 1 second", function () {
     // expectation
     expect(result).toEqual("1s");
   });
+
+// other tests...
+
+test("should return '2s' for 2 seconds", function () {
+    // setup
+    const seconds = 2;
+  
+    // execute
+    const result = time(seconds);
+  
+    // expectation
+    expect(result).toEqual("2s");
+  });
+  
+  test("should return '3s' for 3 seconds", function () {
+    // setup
+    const seconds = 3;
+  
+    // execute
+    const result = time(seconds);
+  
+    // expectation
+    expect(result).toEqual("3s");
+  });
+  
+  test("should return '59s' for 59 seconds", function () {
+    // setup
+    const seconds = 59;
+  
+    // execute
+    const result = time(seconds);
+  
+    // expectation
+    expect(result).toEqual("59s");
+  });
+
+// other tests...
+
+test("should return '1m' for 60 seconds", function () {
+    // setup
+    const seconds = 60;
+  
+    // execute
+    const result = time(seconds);
+  
+    // expectation
+    expect(result).toEqual("1m");
+  });
